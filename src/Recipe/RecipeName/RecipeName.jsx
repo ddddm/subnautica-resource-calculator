61dde44d3b658f965a2358bf068d4c9317b99841
Dmitry Melnikov <m1290d@gmail.com>
2019-08-18 01:24:13 +0500

    remove stupid icon

--- src/Recipe/RecipeName/RecipeName.jsx
+++ src/Recipe/RecipeName/RecipeName.jsx
@@ -2,14 +2,8 @@ import React from "react";
 
 import styles from "./RecipeName.module.css";
 
-function RecipeNameIcon() {
-  return <div className={styles.icon} />;
-}
 export default function RecipeName({ name = "Item" }) {
   return (
-    <>
-      <RecipeNameIcon />
       <h1>{name}</h1>
-    </>
   );
 }
