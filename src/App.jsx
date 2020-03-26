c43f3cc6013bb58a880b94d17a7d710e58a9f37f
Dmitry Melnikov <m1290d@gmail.com>
2020-03-27 00:52:46 +0500

    more itemList inside search and make it overlay

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,17 @@
-import React, { useContext, useState, useCallback, useRef } from "react";
+import React, { useState } from "react";
 
-import ItemsContext from "./itemsContext";
 import Search from "./ItemList/Search";
-import ItemList from "./ItemList/ItemList";
 import RecipeDetails from "./Recipe/RecipeDetails";
 
 import "./globalStyles.css";
 import styles from "./App.module.css";
 
-const getList = materials =>
-  Array.from(materials).map(element => ({
-    token: element[0],
-    name: element[1]
-  }));
-
-const filterItemList = (term, itemsList) => {
-  const t = term.toLowerCase();
-  return itemsList.filter(({ name }) => name.toLowerCase().indexOf(t) !== -1);
-};
-
-const hasTerm = term => {
-  return Boolean(term);
-};
-
 export default function App() {
-  const { materials } = useContext(ItemsContext);
-  const { current: itemsList } = useRef(getList(materials));
-
-  const [term, updateTerm] = useState();
   const [selectedItem, selectItem] = useState();
-  const [isSearchResultsShown, toggleSearchResults] = useState(false);
-
-  const list = (hasTerm(term)
-    ? filterItemList(term, itemsList)
-    : itemsList
-  ).slice(0, 10);
-
-  const handleSearch = useCallback(event => updateTerm(event.target.value), []);
-  const handleItemSelection = useCallback(
-    item => {
-      selectItem(item);
-      toggleSearchResults(false);
-    },
-    [selectItem, toggleSearchResults]
-  );
-  const handleSearchFocus = useCallback(() => {
-    toggleSearchResults(true);
-  }, [toggleSearchResults]);
 
   return (
     <>
-      <Search
-        className={styles.search}
-        onSearch={handleSearch}
-        term={term}
-        onFocus={handleSearchFocus}
-      />
-      {isSearchResultsShown && (
-        <ItemList
-          className={styles.list}
-          list={list}
-          onItemSelect={handleItemSelection}
-        />
-      )}
+      <Search className={styles.search} onItemSelect={selectItem} />
       <RecipeDetails className={styles.details} selectedToken={selectedItem} />
     </>
   );
--- src/ItemList/Search.jsx
+++ src/ItemList/Search.jsx
@@ -1,16 +1,60 @@
-import React from "react";
+import React, { useContext, useState, useCallback, useRef } from "react";
 
+import ItemsContext from "../itemsContext";
+import ItemList from "./ItemList";
 import styles from "./Search.module.css";
+import { cn } from "../utils";
 
-export default function Search({ className, onSearch, term, onBlur, onFocus }) {
+const getList = materials =>
+  Array.from(materials).map(element => ({
+    token: element[0],
+    name: element[1]
+  }));
+const filterItemList = (term, itemsList) => {
+  const t = term.toLowerCase();
+  return itemsList.filter(({ name }) => name.toLowerCase().indexOf(t) !== -1);
+};
+
+const hasTerm = term => {
+  return Boolean(term);
+};
+
+export default function Search({ className, onItemSelect }) {
+  const { materials } = useContext(ItemsContext);
+  const { current: itemsList } = useRef(getList(materials));
+  const [term, updateTerm] = useState();
+  const [isSearchResultsShown, toggleSearchResults] = useState(false);
+
+  const list = (hasTerm(term)
+    ? filterItemList(term, itemsList)
+    : itemsList
+  ).slice(0, 5);
+
+  const handleSearch = useCallback(event => updateTerm(event.target.value), []);
+  const handleItemSelection = useCallback(
+    item => {
+      onItemSelect(item);
+      toggleSearchResults(false);
+    },
+    [onItemSelect, toggleSearchResults]
+  );
+  const handleSearchFocus = useCallback(() => {
+    toggleSearchResults(true);
+  }, [toggleSearchResults]);
   return (
-    <div className={className}>
+    <div className={cn(className, styles.container)}>
       <input
         className={styles.input}
-        onChange={onSearch}
-        onFocus={onFocus}
-        onBlur={onBlur}
+        onChange={handleSearch}
+        onFocus={handleSearchFocus}
       />
+      {isSearchResultsShown && (
+        <ItemList
+          className={styles.list}
+          onItemSelect={handleItemSelection}
+          list={list}
+        />
+      )}
     </div>
   );
 }
