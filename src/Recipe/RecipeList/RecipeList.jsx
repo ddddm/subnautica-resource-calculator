54b7ac904002a7d704a67e9d47608de29de4b427
Dmitry Melnikov <m1290d@gmail.com>
2020-03-26 21:48:37 +0500

    format list

--- src/Recipe/RecipeList/RecipeList.jsx
+++ src/Recipe/RecipeList/RecipeList.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 import RecipeListItem from "../RecipeListItem";
+import classes from "./RecipeList.module.css";
 
 export default function RecipeList({ items, direction }) {
   if (!items) return null;
 
   return (
-    <ul>
+    <ul className={classes.container}>
       {items.map(m => (
         <RecipeListItem key={m.item} direction={direction} material={m} />
       ))}
